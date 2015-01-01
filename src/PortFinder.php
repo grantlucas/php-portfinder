c0b962e75f0c4cf19537e93b89e0bb94602abfcf
Grant Lucas <contact@grantlucas.com>
2015-01-01 10:59:50 -0500

    Added initial PortFinder class

---
+++ src/PortFinder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace GrantLucas;
+
+/**
+ * PortFinder
+ *
+ * A simple tool for finding an open port within a range.
+ */
+class PortFinder
+{
+    public static function range()
+    {
+    }
+}
